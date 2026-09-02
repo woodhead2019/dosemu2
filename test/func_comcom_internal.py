733d403764edab99f4728086d3612e089e8139b0
Andrew Bird <ajb@spheresystems.co.uk>
2026-09-02 23:58:34 +0100

    CI: Add MEM test for comcom internal
    1/ Download mem.zip from FreeDOS base
    2/ Compare internal MEM output against FreeDOS MEM program

--- test/func_comcom_internal.py
+++ test/func_comcom_internal.py
@@ -1,3 +1,104 @@
+import re
+
+from subprocess import check_output
+
+from common_framework import BINSDIR
+
+
+def comcom_mem(self):
+    content = check_output(["unzip", "-p", "-C", f"{self.topdir}/{BINSDIR}/mem.zip", "bin/mem.exe"])
+    fdmem = self.workdir / "fdmem.exe"
+    fdmem.write_bytes(content)
+
+    self.mkfile("testit.bat", """\
+c:\\fdmem
+@echo PT2
+mem
+rem end
+""", newline="\r\n")
+
+    results = self.runDosemu("testit.bat")
+    try:
+        pt2start = results.index("PT2")
+    except ValueError:
+        raise self.failureException("Parse Error:\n" + results) from None
+    pt1 = results[0:pt2start-1]
+    pt2 = results[pt2start+4:]
+    if "Bad command or file name - mem" in pt2:
+        ver = self.__class__.__name__[-2:]
+        raise self.skipTest(f"Internal command 'mem' not available in comcom{ver}")
+
+    def get_triplet(tag, text):
+        s = re.sub(r'[.,]', '', text)
+        plain_match = re.search(fr"^{tag}\s+(\d+)\s+(\d+)\s+(\d+)", s, re.MULTILINE)
+        kilob_match = re.search(fr"^{tag}\s+(\d+)K\s+(\d+)K\s+(\d+)K", s, re.MULTILINE)
+
+        if kilob_match:
+            return [int(x) * 1024 for x in kilob_match.groups()]
+        elif plain_match:
+            return [(int(x) + 1023) & ~1023 for x in plain_match.groups()]
+        else:
+            raise self.failureException(f"Couldn't parse '{tag}' in '{text}'\n")
+
+    def get_single(tag, text):
+        s = re.sub(r'[.,]', '', text)
+        match = re.search(fr"^{tag}\s+.*\((\d+)\s+bytes\)", s, re.MULTILINE)
+        if match:
+            return int(match.group(1))
+        else:
+            raise self.failureException(f"Couldn't parse '{tag}' in '{text}'\n")
+
+    # Parse 'pt1'
+    fd_conv_total, fd_conv_used, fd_conv_free = get_triplet("Conventional", pt1)
+    fd_upper_total, fd_upper_used, fd_upper_free = get_triplet("Upper", pt1)
+    fd_reserved_total, fd_reserved_used, fd_reserved_free = get_triplet("Reserved", pt1)
+    fd_extended_total, fd_extended_used, fd_extended_free = get_triplet(r"Extended \(XMS\)", pt1)
+    fd_total_total, fd_total_used, fd_total_free = get_triplet("Total memory", pt1)
+    fd_1mb_total, fd_1mb_used, fd_1mb_free, = get_triplet("Total under 1 MB", pt1)
+
+    fd_expanded_total = get_single("Total Expanded", pt1)
+    fd_expanded_free = get_single("Free Expanded", pt1)
+    fd_largest_executable = get_single("Largest executable program size", pt1)
+    fd_largest_uppermemblk = get_single("Largest free upper memory block", pt1)
+
+    # Parse 'pt2'
+    cc_conv_total, cc_conv_used, cc_conv_free = get_triplet("Conventional", pt2)
+    cc_upper_total, cc_upper_used, cc_upper_free = get_triplet("Upper", pt2)
+    cc_reserved_total, cc_reserved_used, cc_reserved_free = get_triplet("Reserved", pt2)  # Reserved missing
+    cc_extended_total, cc_extended_used, cc_extended_free = get_triplet(r"Extended \(XMS\)", pt2)
+    cc_total_total, cc_total_used, cc_total_free = get_triplet("Total memory", pt2)
+    cc_1mb_total, cc_1mb_used, cc_1mb_free, = get_triplet("Total under 1 MB", pt2)
+
+    cc_expanded_total = get_single("Total Expanded", pt2)
+    cc_expanded_free = get_single("Free Expanded", pt2)
+    cc_largest_executable = get_single("Largest executable program size", pt2)
+    cc_largest_uppermemblk = get_single("Largest available upper memory block", pt2)
+
+    # compare
+    self.assertAlmostEqual(fd_conv_total, cc_conv_total, delta=1024)
+    self.assertAlmostEqual(fd_conv_free, cc_conv_free, delta=1024)
+    self.assertAlmostEqual(fd_conv_used, cc_conv_used, delta=1024)
+    self.assertAlmostEqual(fd_upper_total, cc_upper_total, delta=1024)
+    self.assertAlmostEqual(fd_upper_free, cc_upper_free, delta=1024)
+    self.assertAlmostEqual(fd_upper_used, cc_upper_used, delta=1024)
+    self.assertAlmostEqual(fd_reserved_total, cc_reserved_total, delta=1024)
+    self.assertAlmostEqual(fd_reserved_free, cc_reserved_free, delta=1024)
+    self.assertAlmostEqual(fd_reserved_used, cc_reserved_used, delta=1024)
+    self.assertAlmostEqual(fd_extended_total, cc_extended_total, delta=1024)
+    self.assertAlmostEqual(fd_extended_free, cc_extended_free, delta=1024)
+    self.assertAlmostEqual(fd_extended_used, cc_extended_used, delta=1024)
+    self.assertAlmostEqual(fd_total_total, cc_total_total, delta=1024)
+    self.assertAlmostEqual(fd_total_free, cc_total_free, delta=1024)
+    self.assertAlmostEqual(fd_total_used, cc_total_used, delta=1024)
+    self.assertAlmostEqual(fd_1mb_total, cc_1mb_total, delta=1024)
+    self.assertAlmostEqual(fd_1mb_free, cc_1mb_free, delta=1024)
+    self.assertAlmostEqual(fd_1mb_used, cc_1mb_used, delta=1024)
+
+    self.assertAlmostEqual(fd_expanded_total, cc_expanded_total, delta=1024)
+    self.assertAlmostEqual(fd_expanded_free, cc_expanded_free, delta=1024)
+    self.assertAlmostEqual(fd_largest_executable, cc_largest_executable, delta=1024)
+    self.assertAlmostEqual(fd_largest_uppermemblk, cc_largest_uppermemblk, delta=1024)
+
 # Note: The Borland/Turbo Pascal binaries are created using the script at
 # the end of this file as I don't want to have to download and install all
 # these compilers for this test, so I'm afraid that we have to just grab
